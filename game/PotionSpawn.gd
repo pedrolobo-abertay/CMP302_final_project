573862d66d61e92987759117f5418c08cff846d2
Pedro Lobo <59931081+pedrolobo-abertay@users.noreply.github.com>
2020-11-03 14:00:57 -0300

    added sprites for potions

--- game/PotionSpawn.gd
+++ game/PotionSpawn.gd
@@ -5,8 +5,15 @@ export var type : String
 signal get_potion
 
 func _ready():
-	pass 
-
+	if type == "Movement":
+		$Sprite.texture = load("res://Assets/red_square.png")
+		$SpritePotion.texture = load("res://Assets/red_potion.png")
+	elif type == "Dash":
+		$Sprite.texture = load("res://Assets/green_square.png")
+		$SpritePotion.texture = load("res://Assets/green_potion.png")
+	elif type == "Invunerability":
+		$Sprite.texture = load("res://Assets/blue_square.png")
+		$SpritePotion.texture = load("res://Assets/blue_potion.png")
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
